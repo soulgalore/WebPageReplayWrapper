973ea68da26e9e9226c94f608b8b7fbeb815fd72
Peter Hedenskog <phedenskog@wikimedia.org>
2017-09-30 21:52:40 +0200

    help and nicer way to start/stop

--- bin/index.js
+++ bin/index.js
@@ -10,13 +10,14 @@ const defaultHTTPSPort = 8081;
 const defaultTmpFile = '';
 
 const argv = minimist(process.argv.slice(2), {
-  boolean: ['start', 'stop', 'record']
+  boolean: ['start', 'stop']
 });
 
 function startStop(wpt, argv) {
-  if (argv.record && argv.stop) {
+  const recordOrReplay = argv._[0];
+  if (recordOrReplay === 'record' && argv.stop) {
     return wpt.stopRecord();
-  } else if (argv.record && argv.start) {
+  } else if (recordOrReplay === 'record') {
     return wpt.startRecord();
   } else if (argv.stop) {
     return wpt.stopReplay();
@@ -26,10 +27,20 @@ function startStop(wpt, argv) {
 }
 
 if (argv.help) {
-  // Add some help here in the future
+  console.log('   Start/stop WebPageReplay record and replay');
+  console.log('   Usage: webpagereplaywrapper replay/record [options]');
+  console.log('   Options:');
+  console.log('   --start           Start the server');
+  console.log('   --stop            Stop the server');
+  console.log('   --http            HTTP port [' + defaultHTTPPort + ']');
+  console.log('   --https           HTTPS port [' + defaultHTTPSPort + ']');
+  console.log(
+    '   --tmp             Path and filename to the file where you store the WPR data'
+  );
+  console.log('   --path            The full path to WebPageReplay directory');
 } else {
   if (!argv.path) {
-    throw Error('Missing path to WebPageReplay');
+    throw Error('Missing the full path to WebPageReplay dir');
   } else {
     const options = {
       httpPort: argv.http || defaultHTTPPort,
